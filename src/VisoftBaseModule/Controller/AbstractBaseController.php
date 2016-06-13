11b49bfe8401602797d6a1d3eb081212a45df59b
vadimizmalkov <399115@gmail.com>
2016-06-13 14:59:38 +0300

    UPDATE

--- src/VisoftBaseModule/Controller/AbstractBaseController.php
+++ src/VisoftBaseModule/Controller/AbstractBaseController.php
@@ -6,7 +6,7 @@ abstract class AbstractBaseController extends \Zend\Mvc\Controller\AbstractActio
 {
 	private $entityManager;
 
-	protected function setEntityManager(EntityManager $entityManager)
+	protected function setEntityManager(\Doctrine\ORM\EntityManager $entityManager)
     {
         $this->entityManager = $entityManager;
         return $this;
@@ -18,5 +18,17 @@ abstract class AbstractBaseController extends \Zend\Mvc\Controller\AbstractActio
             $this->setEntityManager($this->getServiceLocator()->get('Doctrine\ORM\EntityManager'));
         }
         return $this->entityManager;
-    } 
+    }
+
+    protected function redirectToRefer()
+    {
+        $scheme = $this->request->getHeader('Referer')->uri()->getScheme();
+        $host = $this->request->getHeader('Referer')->uri()->getHost();
+        $path = $this->request->getHeader('Referer')->uri()->getPath();
+        $port = $this->request->getHeader('Referer')->uri()->getPort();
+        $port = is_null($port) ? null : ':' . $port;
+        $query = $this->request->getHeader('Referer')->uri()->getQuery();
+        $redirectUrl = $scheme . '://' . $host  . $port . $path . '?' . $query;
+        return $this->redirect()->toUrl($redirectUrl);
+    }
 }
